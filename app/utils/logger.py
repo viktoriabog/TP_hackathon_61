ec6acd6ce6248f0ea2fdc6559a75206dbbd8ab07
ewskyd <chernykh.aleksandra@gmail.com>
2026-06-01 15:13:33 +0300

    Update logger.py

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,7 +1,12 @@
+from datetime import datetime
+
 class Logger:
 
+    def _time(self):
+        return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
     def info(self, message):
-        print(f"[INFO] {message}")
+        print(f"{self._time()} [INFO] {message}")
 
     def error(self, message):
-        print(f"[ERROR] {message}")
+        print(f"{self._time()} [ERROR] {message}")
